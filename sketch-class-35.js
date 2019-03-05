dc397e4785df4064192a7ef41ab72e7da2849e37
saf35 <saf35@duke.edu>
2019-03-05 12:54:27 -0500

    update

--- sketch-class-35.js
+++ sketch-class-35.js
@@ -1,3 +1,17 @@
 /*
 Learning about JSON today
+- With JSON, don't have to reference headers
+
+GET requests
+POST requests
+DELETE requests
+etc.
+*/
+
+function preload(){
+    json = loadJSON(data/data.json);
+}
+
+/*
+json.balls.length: gives number of objects in the json file
 */
